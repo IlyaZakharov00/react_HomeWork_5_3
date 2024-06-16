854dffa3667c1cd39114149e4541f0f06193422e
Ilya frontend <zaxarov.ilya.00@inbox.ru>
2024-06-16 15:43:00 +0500

    gh-pages fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react_HomeWork_5_2",
+  base: "/react_HomeWork_5_3",
   plugins: [react()],
 })
